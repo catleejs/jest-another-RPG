38e06f7167deb1912bd3d65c112172aad8aa8997
Cat <madileesmith@gmail.com>
2021-10-31 16:41:22 -0500

    10.1.6, add potion test

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,10 +1,8 @@
-const { default: expectationResultFactory } = require('jest-jasmine2/build/expectationResultFactory');
-const { test } = require('picomatch');
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
     const potion = new Potion('health');
 
-    expectationResultFactory(potion.name).toBe('health');
-    expectationResultFactory(potion.value).toEqual(expect.any(Number));
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
 });
